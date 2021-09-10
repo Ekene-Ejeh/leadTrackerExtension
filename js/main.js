988c6f13daf8807a624138ba6786708d10c237e8
Ekene Ejeh <ekeneejeh@gmail.com>
2021-09-09 22:52:52 -0700

    added a function that renders the  URL input value on the page and opens the URL link on a new tab

--- js/main.js
+++ js/main.js
@@ -1,18 +1,49 @@
 const inputBtn = document.getElementById("input-btn");
 const inputElement = document.getElementById("input-el");
-let myLeads = ["www.awesomeLead.com", "www.epicLead.com", "www.greatLead.com"];
+let myLeads = [];
 const ulEl = document.getElementById("uList");
 
 inputBtn.addEventListener("click", () => {
 	myLeads.push(inputElement.value);
-	console.log(myLeads);
+	inputElement.value = "";
+	renderLeads();
 });
 
-for (let i = 0; i <= myLeads.length; i++) {
-	ulEl.textContent += myLeads[i] + ", ";
-}
+let renderLeads = () => {
+	let listItems = "";
+	for (let i = 0; i < myLeads.length; i++) {
+		listItems += `
+			<li>
+				<a target='_blank' href='${myLeads[i]}'>
+				${myLeads[i]}
+				</a>
+			</li>`;
+		// 0;
+		//wrap the lead in an anchor tag (<a>) inside the <li>
+		// can you make the link open in a new tab?
+
+		//target='_black'
+
+		// ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+		// const listItem = document.createElement("li");
+		// listItem.textContent = myLeads[i];
+		// ulEl.append(listItem);
+	}
+	ulEl.innerHTML = listItems;
+};
+
+const containerElement = document.getElementById("container");
+containerElement.innerHTML = "<button onClick='buy()' >Buy!</button>";
+
+let buy = () => {
+	containerElement.innerHTML += "<p>Thank you for buying!<p/>";
+};
 
 // let box = document.getElementById("box");
 // box.addEventListener("click", () => {
 // 	console.log("I want to Open the Box!");
 // });
+
+const recipient = "John";
+const email = `Hey ${recipient}! How is it going? Cheers Per`;
+console.log(email);
